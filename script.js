d6aa7d5e0cb6ae769aac0efaaa0bb2e345d84b43
Gus Lascola <augustolascola@gmail.com>
2023-01-27 10:47:35 -0500

    populate quote text and author on refresh--next step is to generate on button click and link to twitter

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-const quoteContainer = document.getElementById('quote-generator')
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
 
 
 // global variables
@@ -7,7 +11,20 @@ let apiQuotes = [];
 function newQuote() { 
     // Pick a random quote from apiQuotes array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    console.log(quote);
+    // Check if author is blank/unknown, if so replace with "Unknown"
+    if (!quote.author) {
+        authorText.textContent = 'Unknown'
+    } else {
+        authorText.textContent = quote.author;
+    }
+
+    // Check if quote text is really long--if so change styling to appropriate CSS styling
+    if (quote.text.length > 100) {
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.remove('long-quote');
+    }
+    quoteText.textContent = quote.text;
 }
 
 // Get quotes from API 
